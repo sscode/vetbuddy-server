be573074610253c53396066eb5c71b0c943b9da4
Stuart Sim <stuartsim.aus@gmail.com>
2023-10-30 15:40:48 -0700

    clean

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ admin.initializeApp({
     // Add your Firebase Admin SDK credentials here
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  databaseURL: process.env.FIREBASE_DATABASE_URL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
 }),
 });
 
