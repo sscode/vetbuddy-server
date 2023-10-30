6a5736d0b836ee0368bd6ced39b3f95f6f01be2f
Stuart Sim <stuartsim.aus@gmail.com>
2023-10-30 15:37:02 -0700

    get collections from fb

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const axios = require('axios');
 const sgMail = require('@sendgrid/mail');
-const cron = require('node-cron');
+const admin = require('firebase-admin');
 require('dotenv').config()
 const cors = require('cors');
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert({
+    // Add your Firebase Admin SDK credentials here
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  databaseURL: process.env.FIREBASE_DATABASE_URL,
+}),
+});
 
 const app = express();
 const port = 5050;
@@ -44,25 +54,91 @@ app.get('/stockQuote', async (req, res) => {
 });
 
 
-app.get('/send-email', async (req, res) => {
+// Define a function to send the email
+async function sendEmail(portfolioData) {
   try {
     const text = 'This is a scheduled email Vercel.';
+    const msg = {
+      to: 'stuartsim.aus+trainingstats@gmail.com',
+      from: 'stuartsim.aus@gmail.com',
+      subject: 'Logging Connection Vercel',
+      html: generateEmailContent(portfolioData), // Replace with your email content
+    };
 
-      const msg = {
-          to: 'stuartsim.aus+trainingstats@gmail.com',
-          from: 'stuartsim.aus@gmail.com',
-          subject: 'Logging Connection Vercel',
-          html: `<strong>${text}</strong>`, // Replace with your email content
-      };
+    await sgMail.send(msg);
 
-      await sgMail.send(msg);
-
-      console.log('Email sent');
-      res.send('Email sent successfully');
+    console.log('Email sent');
   } catch (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send('Error sending email');
+    console.error('Error sending email:', error);
+    throw error; // Re-throw the error for handling at a higher level if needed
+  }
+}
+
+function generateEmailContent(portfoliosData) {
+  let emailContent = ''; // Initialize the email content
+
+  // Loop through each portfolio in the response
+  portfoliosData.forEach((portfolio) => {
+    emailContent += `<h2>${portfolio.name}</h2>`; // Portfolio name as a heading
+    emailContent += '<table border="1">'; // Start a table
+
+    // Loop through each stock in the portfolio
+    portfolio.stocks.forEach((stock) => {
+      emailContent += `
+        <tr>
+          <td>${stock.stock}</td> <!-- Stock symbol -->
+          <td>${stock.quantity}</td> <!-- Quantity -->
+        </tr>
+      `;
+    });
+
+    emailContent += '</table>'; // End the table for the portfolio
+  });
+
+  return emailContent;
+}
+
+// Endpoint to fetch subcollections for a user
+app.get('/user/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    // Get references to the "portfolios" and "stocks" subcollections
+    const portfoliosCollection = admin.firestore().collection('users').doc(userId).collection('portfolios');
+    const stocksCollection = admin.firestore().collection('users').doc(userId).collection('stocks');
+
+    // Fetch portfolios with emailAlerts set to true
+    const portfoliosQuerySnapshot = await portfoliosCollection.where('emailAlerts', '==', true).get();
+    const portfoliosData = [];
+
+    // Iterate over portfolios with emailAlerts
+    for (const portfolioDoc of portfoliosQuerySnapshot.docs) {
+      const portfolioData = portfolioDoc.data();
+      portfolioData.stocks = [];
+
+      // Fetch associated stocks for this portfolio
+      const stocksQuerySnapshot = await stocksCollection.where('portfolioId', '==', portfolioDoc.id).get();
+      stocksQuerySnapshot.forEach((stockDoc) => {
+        const stockData = stockDoc.data();
+        // Include only basic information for each stock
+        portfolioData.stocks.push({ stock: stockData.stock, quantity: stockData.quantity });
+      });
+
+      // Include portfolio name and associated stocks in the response
+      if (portfolioData.stocks.length > 0) {
+        portfoliosData.push({ name: portfolioData.name, stocks: portfolioData.stocks });
+      }
+    }
+
+    // Send the email
+    await sendEmail(portfoliosData);
+
+    res.json(portfoliosData);
+  } catch (error) {
+    console.error('Error fetching portfolios with alerts:', error);
+    res.status(500).send('Error fetching portfolios with alerts');
   }
 });
+
 
 
