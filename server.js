a40aec3678ad7d85542e213efbdf1d54ceea1313
Stuart Sim <stuartsim.aus@gmail.com>
2023-11-01 16:32:42 -0700

    remove email blanks

--- server.js
+++ server.js
@@ -142,9 +142,9 @@ async function sendEmailToUser(userId) {
       }
     }
 
-    if(userEmail !== 'stuartsim.aus+firebase@gmail.com'){
-      userEmail = 'stuartsim.aus+alternate@gmail.com'
-    }
+    // if(userEmail !== 'stuartsim.aus+firebase@gmail.com'){
+    //   userEmail = 'stuartsim.aus+alternate@gmail.com'
+    // }
 
     // Send the email
     await sendEmail(portfoliosData, userEmail);
