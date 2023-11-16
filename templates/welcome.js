7a86b52b516a64226dd33a75d85e1324bb6a54da
Stuart Sim <stuartsim.aus@gmail.com>
2023-11-16 15:41:53 -0800

    welcome copy + css

--- templates/welcome.js
+++ templates/welcome.js
@@ -1,11 +1,11 @@
 const welcome = `
-<table style="padding: 240px 24px; text-align: center; background-color: #F7F7F7; text-align: center;" border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+<table style="padding: 120px 24px; text-align: center; background-color: #F7F7F7; text-align: center;" border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
     <tbody>
         <tr>
             <td>
-                <p class="subtext">Welcome to Finlister. Please confirm your email</p>
+                <p class="subtext">Please confirm your email to receive your portfolio updates.</p>
                 <button
-                style="margin-top: 24px;"
+                style="margin-top: 12px; background-color: #000; color: #F7F7F7; border: none; padding: 12px 24px; border-radius: 4"
                 >Confirm</button>
             </td>
         </tr>
