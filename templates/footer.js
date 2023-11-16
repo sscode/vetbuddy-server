2fe0df44c52931b78f5072ca1f7933c0640ccee6
Stuart Sim <stuartsim.aus@gmail.com>
2023-11-16 15:18:35 -0800

    footer udpdate

--- templates/footer.js
+++ templates/footer.js
@@ -1,14 +1,28 @@
-const footer = `<tbody>
-    <tr>
-        <td>
-            <table width="100%" style="width: 166px; margin: auto;" align="center" role="presentation" cellpadding="0" cellspacing="0" border="0">
-                <tbody></tbody>
-            </table>
-            <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center; padding-top: 30px; padding-bottom: 30px; color: #000;">Please contact us if you have any questions.</p>
-            <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center;">© 2023 Finlister, Inc. All Rights Reserved.</p>
-            <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center;">Finlister, INC.</p>
-        </td>
-    </tr>
-</tbody>`;
+const footer = `
+                <table style="padding-top: 22px; padding-bottom: 22px;" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p 
+                                style="color: #000;"
+                                class="section-heading">Finlister.com</p>
+                                <table width="100%" style="width: 20px; margin: auto; padding-top: 12px;" align="center" role="presentation" cellpadding="0" cellspacing="0" border="0">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center"><a target="_blank" href="https://www.linkedin.com/in/simstuart"><img src="https://res.cloudinary.com/dfvcq2b/image/upload/v1699288939/image-1_vhaoq2.png" width="16px" height="16px" style="display: block; outline: none; border: none; text-decoration: none; padding-right: 14px;" /></a></td>
+                                            <td align="center"><a target="_blank" href="https://twitter.com/stusim"><img src="https://res.cloudinary.com/dfvcq2b/image/upload/v1699288939/image-2_raeybe.png" width="16px" height="16px" style="display: block; outline: none; border: none; text-decoration: none; padding-right: 14px;" /></a></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center; padding-top: 30px; padding-bottom: 30px; color: #000;">Please contact us if you have any questions.</p>
+                                <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center;">© 2023 Finlister, Inc. All Rights Reserved.</p>
+                                <p style="font-size: 13px; line-height: 24px; margin: 0; color: #AFAFAF; text-align: center;">Finlister, INC.</p>                    
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+`;
 
 module.exports = footer;
