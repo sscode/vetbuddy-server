2688a599007fe2d7a192a74269460be0faf4bf61
Stuart Sim <stuartsim.aus@gmail.com>
2024-02-26 17:06:43 +1100

    prompt update

--- api/prompt.js
+++ api/prompt.js
@@ -14,7 +14,7 @@ const textPromptEnd = `
     
     If there is a section that is marked with **SPECIAL INSTRUCTIONS** then there are special circumstances. If the area of concern is not mentioned in the consult then return the default values that are included in this section, otherwise overwrite the default values with the new information from the consult. 
 
-    If there is any information missing, write that more information is needed except in the case of sections marked **SPECIAL INSTRUCTIONS**. Do not include anything is unrelated to the reason for the visit and does not provide any medical information about the animal.
+    If there is any information missing, write nothing except for the section name, except in the case of sections marked **SPECIAL INSTRUCTIONS**. Do not include anything is unrelated to the reason for the visit and does not provide any medical information about the animal.
     
     Keep the notes succinct and to the point. Do not reference "The Vet" in third person. Remove any references to **SPECIAL INSTRUCTIONS**
     `;
